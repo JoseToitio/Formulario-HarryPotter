ae41e7221e500f64cad9ce900ea19d4083294856
Jose <josetoitio@gmail.com>
2021-09-22 19:11:21 -0300

    Requisito 14 completo

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ document.querySelector('#button-form').addEventListener('click', () => {
     alert('Email ou senha inválidos.');
   }
 });
+
+const divAva = document.querySelector('#div-avaliador');
+const numButton = 10;
+
+function criaLabel() {
+  const label = document.querySelector('label');
+  label.innerHTML = 'Como você avalia a Trybewarts?';
+  label.id = 'label-rate';
+  divAva.appendChild(label);
+}
+criaLabel();
+
+function criaButton() {
+  for (let index = 1; index <= numButton; index += 1) {
+    const input = document.createElement('input');
+    const label = document.createElement('label');
+    label.innerHTML = `<label for="${index}">${index}</label>`;
+    input.value = index;
+    input.id = index;
+    input.type = 'radio';
+    input.name = 'rate';
+    divAva.appendChild(input);
+    divAva.appendChild(label);
+  }
+}
+criaButton();
